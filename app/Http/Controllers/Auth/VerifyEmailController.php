aaa80dffa7d87d20874fb9d8ddc86d171cbf83e2
rizkychi <rizkynhae@gmail.com>
2025-08-30 20:17:29 +0700

    feat: Refactor authentication views and add verified email page

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    public function __invoke(EmailVerificationRequest $request): RedirectResponse|View
     {
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
@@ -22,6 +23,7 @@ class VerifyEmailController extends Controller
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+        return view('auth.verified-email');
+        // return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,19 +2,7 @@
 
 <x-guest-layout>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="text-center mt-sm-5 mb-4 text-white-50">
-                <div>
-                    <a href="index.html" class="d-inline-block auth-logo">
-                        <img src="{{ Vite::asset('resources/images/logo-light.png') }}" alt="" height="20">
-                    </a>
-                </div>
-                <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
-            </div>
-        </div>
-    </div>
-    <!-- end row -->
+    <x-login-title />
 
     <div class="row justify-content-center">
         <div class="col-md-8 col-lg-6 col-xl-5">
